cf9ce9f5832cb58a1037b261c3f68375c44c8fa2
AAYUS KARKI <alexkarki2060@gmail.com>
2024-02-21 17:56:27 +0545

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { useState } from "react"
 
 
 function App() {
 
-
+  let [counter, setcounter] = useState(0);
+  
+  const addvalue=()=>{
+    if(counter<5)
+    setcounter(counter+1)
+  else{
+    alert("ONLY UPTO 5 AVAILABE TO ADD")
+  }}
   return (
     <>
+    
       <h1>AAYUS KARKI</h1>
+      <button onClick={addvalue}>The count is {counter}</button>
     </>
   )
 }
